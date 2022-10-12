8a6959e534149cb07ca316eca2930cfa83906c53
Mauricio Monti <mauricio.monti@cobmais.com.br>
2022-10-12 19:41:45 -0300

    event-ignite-reactjs

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+
+import client from "./lib/apollo";
+import { Router } from "./Router";
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
+}
+
+export default App;
